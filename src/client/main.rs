e60d5d2888227253e9fd6ffdd0e8ad7afb16224f
JIEEEN <tjtpp009@korea.ac.kr>
2024-08-04 22:41:24 +0900

    mod main

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use sysinfo::{System, SystemExt};
+
+fn main() {
+    let mut system = System::new_all();
+    system.refresh_all();
+
+    let os_name = system
+        .name()
+        .unwrap_or_else(|| "os 정보 확인 불가".to_string());
+    let os_version = system
+        .long_os_version()
+        .unwrap_or_else(|| "os version 정보 확인 불가".to_string());
+}
--- src/master/main.rs
+++ src/master/main.rs
@@ -8,13 +8,17 @@ fn init_logger() -> Result<(), SetLoggerError> {
     log::set_logger(&LOGGER).map(|()| log::set_max_level(log::LevelFilter::Debug))
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let _logger_init = init_logger().unwrap();
 
     let server_ip: String = String::from("0.0.0.0");
     let server_port: String = String::from("8080");
-    let server = server::server::Server::new(server_ip, server_port);
+    let db_ip: String = String::from("127.0.0.1");
+    let dp_port: String = String::from("27017");
 
-    let listener = server.init();
+    let server = server::server::Server::new(server_ip, server_port, db_ip, dp_port);
+
+    let listener = server.init().await;
     server.run(listener);
 }
