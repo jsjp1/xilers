94a8c0034b01316bc897e4bd6bbd6887029e525f
JIEEEN <tjtpp009@korea.ac.kr>
2024-08-04 11:39:32 +0900

    add os_version to spec, sys_info

--- src/device/spec.rs
+++ src/device/spec.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize)]
 pub struct DeviceSpec {
     pub ip: String,
-    pub port: u16,
+    pub port: u16, // 어떤 port로 listen하고 있는지
     pub os: String,
+    pub os_version: String,
 }
