6a32d9f34f64eef3fe504c1c523fe2a935be4900
JIEEEN <tjtpp009@korea.ac.kr>
2024-08-21 00:49:15 +0900

    add client test code

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,6 +1,9 @@
 use sysinfo::{System, SystemExt};
 
-fn main() {
+use device::device::spec::DeviceSpec;
+
+#[tokio::main]
+async fn main() {
     let mut system = System::new_all();
     system.refresh_all();
 
@@ -10,4 +13,32 @@ fn main() {
     let os_version = system
         .long_os_version()
         .unwrap_or_else(|| "os version 정보 확인 불가".to_string());
+
+    let a: DeviceSpec = DeviceSpec {
+        ip: "11".to_string(),
+        os: os_name,
+        os_version: os_version,
+    };
+
+    let client = reqwest::Client::new();
+    let uuid = client
+        .post("http://xilers.jieeen.co.kr:8080/device-manager/")
+        .body("")
+        .send()
+        .await
+        .unwrap();
+    // 응답 본문을 텍스트로 변환
+    let body = uuid.text().await.unwrap();
+    let spec = serde_json::to_string(&a).unwrap();
+    let s = format!(r"{}", spec);
+
+    let c = format!(
+        "http://xilers.jieeen.co.kr:8080/device-manager/{}/spec",
+        body
+    );
+    let d = client.post(c).body(s).send().await.unwrap();
+    let aaa = d.text().await.unwrap();
+
+    // 응답 출력
+    println!("Response: {:?}", aaa);
 }
