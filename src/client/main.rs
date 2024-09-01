3082e4a6bd44bf8cf63f324c1176fb865c5b6c71
JIEEEN <tjtpp009@korea.ac.kr>
2024-09-01 09:01:04 +0900

    add client config file

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,22 +1,50 @@
-use signal_hook::consts::SIGINT;
 use std::env;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
+use serde::Deserialize;
 
 mod network;
 mod ui;
-use network::tcp::network::TcpNetwork;
 use ui::cli::interface::Cli;
 use ui::interface::Interface;
 
+#[derive(Debug, Deserialize)]
+struct Config {
+    server: ServerConfig,
+    client: ClientConfig,
+}
+
+#[derive(Debug, Deserialize)]
+struct ServerConfig {
+    master_ip: String,
+    master_port: u16,
+}
+#[derive(Debug, Deserialize)]
+struct ClientConfig {
+    file_storage: String,
+    listen_port: u16,
+}
+
 #[tokio::main]
 async fn main() {
-    let master_ip = "http://xilers.jieeen.kr";
-    let master_port = 8080;
-    let listen_port = 8081;
-    let file_storage = "/Users/jin/Desktop/test";
-
-    let master_addr = format!("{}:{}", master_ip, master_port);
+    let config_content: Config = match std::fs::read_to_string("config.toml") {
+        Ok(config_str) => {
+            toml::from_str(&config_str).expect("config.toml파일을 파싱하는데 문제가 발생했습니다. 파일 내용을 확인하시기 바랍니다.")
+        },
+        Err(e) => {
+            println!("파일이 존재하지 않습니다. 기본 설정을 적용합니다: {}", e.to_string());
+            Config {
+                server: ServerConfig {
+                    master_ip: String::from("127.0.0.1"),
+                    master_port: 8080,
+                },
+                client: ClientConfig {
+                    // TODO: os별 다른 기본 file_storage
+                    file_storage: String::from("/tmp"),
+                    listen_port: 8081,
+                },
+            }
+        }
+    };
+    let master_addr = format!("{}:{}", config_content.server.master_ip, config_content.server.master_port);
 
     if env::args().len() == 1 {
         println!("usage: ./client [cli | gui]");
@@ -26,7 +54,7 @@ async fn main() {
     // TODO: ui 옵션 파싱 방식 변경
     if env::args().nth(1).unwrap() == "cli" {
         println!("CLI mode로 시작합니다.");
-        let mut cli: Cli = Interface::new(master_addr, listen_port, file_storage.to_string());
+        let mut cli: Cli = Interface::new(master_addr, config_content.client.listen_port, config_content.client.file_storage.to_string());
         cli.entry().await;
     } else if env::args().nth(1).unwrap() == "gui" {
         println!("GUI mode로 시작합니다.");
